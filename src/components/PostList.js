e1895aee3c738a3da1ddbde8a6976bb40ab2e9cf
Gushchina Viktoria <piterbulka@mail.ru>
2023-09-28 03:26:45 +0300

    update postList

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -4,37 +4,42 @@ import { useNavigate } from 'react-router-dom';
 import PostListItem from './PostListItem';
 import axios from 'axios';
 
-const PAGE_SIZE = 10;
-
-const fetchPosts = async (key, page = 1) => {
-  const response = await axios.get(
-    `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${PAGE_SIZE}`
-  );
-  return response.data;
-};
-
 const PostsList = () => {
-  const navigate = useNavigate(); 
+  const navigate = useNavigate();
+  const PAGE_SIZE = 10;
+
+  const fetchPosts = async ({ pageParam = 1 }) => {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts?_page=${pageParam}&_limit=${PAGE_SIZE}`
+    );
+    return response.data;
+  };
 
   const {
     data,
     fetchNextPage,
     hasNextPage,
     isFetchingNextPage,
+    isLoading,
+    isError,
     error,
   } = useInfiniteQuery('posts', fetchPosts, {
-    getNextPageParam: (lastPage, allPages) => {
-      const nextPage = lastPage.length + 1;
-      return nextPage <= allPages.length ? nextPage : false;
+    getNextPageParam: (lastPage, pages) => {
+      const nextPage = pages.length + 1;
+      return nextPage;
     },
   });
 
-  if (error) {
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
     return <div>Error: {error.message}</div>;
   }
 
   const handlePostClick = (postId) => {
-    navigate(`/post/${postId}`); // Используйте navigate для перехода
+    navigate(`/post/${postId}`);
   };
 
   return (
@@ -55,7 +60,7 @@ const PostsList = () => {
           onClick={() => fetchNextPage()}
           disabled={isFetchingNextPage}
         >
-          {isFetchingNextPage ? 'Загрузка...' : 'Загрузить еще'}
+          {isFetchingNextPage ? 'Loading more...' : 'Load More'}
         </button>
       )}
     </div>
